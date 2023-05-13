bca298297891056b2fd21d3dded9a5b04b77eb08
zouhairnasser-admin <zouhairnasser@warshatec.com>
2023-05-13 11:19:18 +0000

    init

---
+++ pages/posts/[id].tsx
@@ -0,0 +1,47 @@
+import Layout from "@/components/layout";
+import Head from "next/head";
+import Date from "@/components/date";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+import utilStyles from "@/styles/utils.module.css";
+export async function getStaticProps({ params }: { params: { id: string } }) {
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export default function Post({
+  postData,
+}: {
+  postData: {
+    title: string;
+    id: string;
+    date: string;
+    contentHtml: string;
+  };
+}) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
+}
